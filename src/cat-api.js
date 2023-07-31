f7b57f062d768e1f6c143b1f63ce6dcfabd41ad0
Arysill <125421389+Arysill@users.noreply.github.com>
2023-07-30 20:54:30 -0500

    jk

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,12 +1,18 @@
 import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_793CeG5q67qnH6fpJBwdr6YWNaVcGC2tcdHB4T47MUXXJN1CFrX7Fw14EMbW77uI';
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
 
-export function fetchBreeds() {
-  return axios.get('breeds').then(({ data }) => {
-    return data;
+
+export function fetchBreeds(endPoint) {
+  return axios.get(endPoint).then(data => {
+    return data.data;
   });
 }
-export function fetchCatByBreed(breedId) {
-  return axios.get(`images/search?breed_ids=${breedId}`).then(({ data }) => {
-    return data;
+
+export function fetchCatByBreed(endPoint, breedId) {
+  const catByBreed = endPoint + '?breed_ids=' + breedId;
+  return axios.get(catByBreed).then(resp => {
+    return resp.data;
   });
 }
